0addf457fd6d48abce7fac137bf2ba3b86e7baf9
Daniel <danielpellegrini87@gmail.com>
2021-01-11 16:42:36 +0100

    Difficulty level choice added

--- js/script.js
+++ js/script.js
@@ -40,19 +40,54 @@ con difficoltà 2 => tra 1 e 50
 */
 
 
-
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
+while (diffLvl < 0 || diffLvl > 2 || isNaN(diffLvl)) { //checking if the user is entering valid input
+  var diffLvl = parseInt(prompt('Choose the difficulty level:\n0 - Easy\n1 - Medium\n2 - Hard'));
+  if (diffLvl < 0) {
+    alert('You cannot enter a number lower than 0');
+  } else if (diffLvl > 2) {
+    alert('You cannot enter a number higher than 2')
+  } else if (isNaN(diffLvl)) {
+    alert('Invalid input.\nTry Again')
+  }
+}
 
 var bombsList = []; //empty array for 'bomb numbers'
+var value = "";
+var maxValue;
 
 
 while (bombsList.length < 16) {
-  var randomNum = randomNums(1, 100);
 
-  if (!isPresent(bombsList, randomNum)) {
-    bombsList.push(randomNum);
+  switch (diffLvl) {
+    case 0:
+      value = randomNums(1, 100);
+      maxValue = 100;
+      console.log('max value: ' + maxValue);
+      break;
+    case 1:
+      value = randomNums(1, 80);
+      maxValue = 80;
+      console.log('max value: ' + maxValue);
+      break;
+    case 2:
+      value = randomNums(1, 50);
+      maxValue = 50;
+      console.log('max value: ' + maxValue);
+      break;
+    default:
+      value = randomNums(1, 100);
+      maxValue = 100;
+      console.log('max value: ' + maxValue);
+      break;
+  }
+  var comparedNums = isPresent(value, bombsList);
+
+  if (comparedNums === false) {
+    bombsList.push(value);
   }
 }
+
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
 
 bombsList.sort(function(a, b) {
   return a - b
@@ -62,12 +97,12 @@ console.log(bombsList);
 
 // Chiedere all’utente (100 - 16) per 84 volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 
-var maxValue = 100;
+// var maxValue = 100;
 var userList = [];
 blowedBomb = false;
 
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-while (userList.length < maxValue - 98 && blowedBomb === false) { //exit condition from while cycle if user hit a bomb or guesses every correct number
+while (userList.length < maxValue - 16 && blowedBomb === false) { //exit condition from while cycle if user hit a bomb or guesses every correct number
 
   do {
 
@@ -112,18 +147,18 @@ console.log('User\'s numbers list:', userList);
 
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-if (userList.length === maxValue - 98) {
+if (userList.length === maxValue - 16) {
   alert('No more numbers left! You won!');
   console.log('You won!');
 }
 
 if (userList.length <= 5) {
-  alert('Puah! You suck! Your Score is: ' + parseInt(userList.length) );
-  console.log('Puah! You suck! Your Score is: ' + parseInt(userList.length) );
+  alert('Puah! You suck! Your score is: ' + parseInt(userList.length)  + ' points');
+  console.log('Puah! You suck! Your score is: ', userList.length  + ' points');
 } else if (userList.length < 20) {
   alert('Hmm... could be better.\nYour score is: ' + parseInt(userList.length) + ' points');
   console.log('Hmm... could be better.\nYour score is: ', userList.length);
 } else {
   alert('Well done! Your score is: ' + parseInt(userList.length) + ' points');
-  console.log('Well done! Your score is:', userList.length);
+  console.log('Well done! Your score is:', userList.length  + ' points');
 }
